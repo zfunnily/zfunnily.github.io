92b5b5684bb2a92380b419f5b923d60ead800907
zqj <582132116@qq.com>
2021-10-22 14:49:53 +0800

    update

--- js/totop.js
+++ js/totop.js
@@ -10,3 +10,31 @@ $("#rocket").click(function() {
     });
     return false;
 });
+
+function randomQuotesIdx(start =0, end = 100) {
+    return parseInt((end - start + 1) * Math.random() + start);
+}
+
+function getQuotesJsonArray(file, callback) {
+    $.getJSON(file, function(data){
+        callback(data)
+    });
+}
+
+$(document).ready(function (){
+    // var quotes = [
+    //     ["时来谁不来,时不来谁来","兰陵笑笑声", "《金瓶梅》"],
+    //     ["投资成功与否并非取决于你了解的东西，而在于你能否老老实实地承认你所不知道的东西！\n" +
+    //     "投资人并不需要做对很多事情，重要的是不要犯重大的错误！", "巴菲特"],
+    //     ["好的程序代码本身就是最好的文档", "Steve McConnell", "《代码大全》"],
+    // ]
+    getQuotesJsonArray("/js/quotes.json", function (quotes) {
+        var idx = randomQuotesIdx(0,quotes.length-1)
+        var quote = quotes[idx]
+        var tmpHtml
+        if (quote !== undefined) {
+            tmpHtml = quote[0] +  " —— " + quote[1] + " " + (quote[2] ? "《"+quote[2] +"》": "");
+        }
+        $("#quotes").text(tmpHtml)
+    })
+})
