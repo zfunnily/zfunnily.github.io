eefdab23e652cecd9d52ce0adabdb5fdcaa0bdc4
zqj <582132116@qq.com>
2021-12-22 23:18:50 +0800

    update

--- js/totop.js
+++ js/totop.js
@@ -42,4 +42,27 @@ $(document).ready(function (){
         }
         document.getElementById('quotes').innerText = tmpHtml
     })
+
+    //暗主题跟主题切换
+    const options = {
+        bottom: '64px', // default: '32px'
+        right: 'unset', // default: '32px'
+        left: '32px', // default: 'unset'
+        time: '0.5s', // default: '0.3s'
+        mixColor: '#fff', // default: '#fff'
+        backgroundColor: '#fff',  // default: '#fff'
+        buttonColorDark: '#100f2c',  // default: '#100f2c'
+        buttonColorLight: '#fff', // default: '#fff'
+        saveInCookies: false, // default: true,
+        label: '🌓', // default: ''
+        autoMatchOsTheme: true // default: true
+    }
+
+    function addDarkmodeWidget() {
+        const darkmode = new Darkmode(options)
+        // darkmode.toggle()
+        darkmode.showWidget()
+        console.log(darkmode.isActivated())
+    }
+    window.addEventListener('load', addDarkmodeWidget);
 })
