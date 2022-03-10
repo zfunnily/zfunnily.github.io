1fcf08a82c8a8e5466a3410ca06b9d7032d746dd
zqj <582132116@qq.com>
2022-03-10 16:09:49 +0800

    update

--- js/totop.js
+++ js/totop.js
@@ -62,26 +62,4 @@ $(document).ready(function (){
     //     darkmode.showWidget()
     // }
     // window.addEventListener('load', addDarkmodeWidget);
-
-    //复制按钮
-   $(".code-detail").each(function(){
-    var t = $(this).children("highlight").text();
-    var btn = $('<span class="copy">copy</span>').attr(
-        "data-clipboard-text",
-        t
-      );
-    $(this).prepend(btn);
-    var c = new ClipboardJS(btn[0]);
-    c.on("success", function() {
-      btn.addClass("copyed").text("success");
-    });
-    c.on("error", function() {
-      btn.text("fail");
-    });
-    btn.mouseleave(function() {
-      btn.text("copy").removeClass("copyed");
-    });
-    document.getElementsByClassName('code-detail').addEventListener('click', copyArticle, false);
-   })
-    
 })
